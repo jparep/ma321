54d02068bc36e09871e75f0a45f823d225c0fdb0
joshuaparep <parepsonjopa@gmail.com>
2023-03-09 17:13:00 +0000

    Regression Model added

--- house_price.R
+++ house_price.R
@@ -91,17 +91,16 @@ num_df <- sub_df[num_df]
 ################ HANDLING NAs - IMPUTAE REMAINING MISSING VALUES ##############
 #impute NAs - In this case, the random forest mice function is used. Random m set to 5
 imp_df <- mice(df1, seed = 123, m=5, method = "rf")
-comppleted_imp_df <- complete(imp_df,3) # use 3rd cycle complete imputed dataset
+df0 <- complete(imp_df,3) # use 3rd cycle complete imputed dataset
 
-summary(complete(comppleted_imp_df)) ##Summary for Descriptive Statistical Analysis
+summary(complete(df0)) ##Summary for Descriptive Statistical Analysis
 
-class(comppleted_imp_df)
-unique(comppleted_imp_df$OverallCond)
+class(df0)
+unique(df0$OverallCond)
 
 # Check for any NA after imputation?
-sapply(comppleted_imp_df, function(x) sum(is.na(x))) # good to go!
-sum(is.na(comppleted_imp_df))
-df0 <- comppleted_imp_df
+sapply(df0, function(x) sum(is.na(x))) # good to go!
+sum(is.na(df0))
 
 ########## CORRELATION ########################################################
 #Correlation with 
@@ -173,12 +172,19 @@ comppleted_imp_df %>% select(MiscVal) %>%  plot_outlier()     t
 ###############################################################################
 #install.packages("DataExplorer")
 library(DataExplorer)
-plot_correlation(na.omit(comppleted_imp_df), maxcat = 5L)
+plot_correlation(na.omit(df0), maxcat = 5L)
 
-df_scaled <- comppleted_imp_df %>% mutate_if(is.numeric, scale)
+library(corrplot)
+data.corr <- as.data.frame(sapply(df, as.numeric))
 
-summary(df_scaled)
+correlations = cor(data.corr, method = "s")
+# Show variables that have strong correlations with price, focus on coefficient > 0.5 or < -0.5
+corr.price = as.matrix(sort(correlations[,'SalePrice'], decreasing = TRUE))
+corr.id = names(which(apply(corr.price, 1, function(x) (x > 0.05 | x < -0.50))))
+corrplot(as.matrix(correlations[corr.id,corr.id]), type = 'upper', method='color', addCoef.col = 'black', tl.cex = 1,cl.cex = 1, number.cex=1)
 
+
+sapply(df, function(x) length(unique(x)))
 
 
 #############################################################################
@@ -193,162 +199,170 @@ num_df <- comppleted_imp_df[, num_df]
 colSums(num_df == 0)
 
 ######### SUBSET CATEGORICAL VARIABLEA ########
-cat_df <- unlist(lapply(comppleted_imp_df, is.factor))
-cat_df <- comppleted_imp_df[, cat_df]
+cat_df <- unlist(lapply(df0, is.factor))
+cat_df <- df0[, cat_df]
 str(cat_df)
-
-
 
 # TO DO - NEED TO SELECT FEATURES FIRST
 count(comppleted_imp_df, OverallCond) #Check the classification distribution
 
+
 ############## Training Data ###############################
-#create a list of random number ranging from 1 to number of rows from actual data 
-#and 80% of the data into training data 
 
-#Multinomial Logistic regression
-library(nnet)
-comppleted_imp_df$OverallConClass<-relevel(comppleted_imp_df$OverallConClass, ref="1")
-mymodel<-multinom(OverallConClass~., data=comppleted_imp_df)
-summary(mymodel)
-
-#2-tailed z test
-z <- summary(mymodel)$coefficients/summary(mymodel)$standard.errors
-p <- (1 - pnorm(abs(z), 0, 1)) * 2
-p
-
-#d <- comppleted_imp_df
-#mylogit <- glm(d$OverallConClass ~., data = d, family = "binomial")
-
-summary(mylogit)
-# Multinomial Logistic Regression - using mglogit
-#install.packages("mlogit")
-library("mlogit")
-df3 <- mlogit.data(comppleted_imp_df, varying = NULL, choice = "OverallConClass", shape="wide")
-head(df3)
-
-model <- mlogit(df3$OverallConClass ~1, data = df3, reflevel = "1")
-summary(model)
-
-
-#############
-xtabs(OverallCond ~., data=df0, sparse=TRUE)
-
-xtabs(~ df0$OverallCond + df0$OverallQual, data=df0)
-
-logistic <- glm(OverallCond ~., data=df0, family="binomial")
-summary(logistic)
-
-
-####STEPWISE###
-base.mod <- glm(OverallCond ~ 1 , data= df0, family="binomial")  # base intercept only model
-all.mod <- glm(OverallCond ~ . , data= df0, family="binomial") # full model with all predictors
-
-
-############## Log Reg ##
+###### Train 1 #####
+set.seed(1)
 index <- sample(nrow(df0),nrow(df0)*0.80)
-credit_train = df0[index,]
-credit_test = df0[-index,]
+train = df0[index,]
+test = df0[-index,]
+
+## Train 2 #####
+#install.packages("caTools")
+library(caTools)
+set.seed(12)
+split <- sample.split(OverallCond, SplitRatio = 0.8)
+train <- subset(df0, split == T)
+test <- subset(df0, split == F)
+# check output Class distributiion
+table(test$OverallCon)
+pre <- predict(mod3, newdata=train, type = "response",)
+#table(test$OverallCond, pre > 0.5)
+y_pred_num <- ifelse(pre > 0.5, 1, 0)
+y_pred <- factor(y_pred_num, levels=c(0, 1))
+y_act <- test$OverallCond
+table(train$OverallCond, pre > 0.5)
+
+########### TRain 3 #####################
+#install.packages("caret")
+library(caret)
+'%ni%' <- Negate('%in%')  # define 'not in' func
+options(scipen=1)  # prevents printing scientific notations.
+set.seed(100)
+split <- createDataPartition(df0$OverallCond, p=0.8, list = F)
+train <- df0[split, ]
+test <- df0[-split, ]
+
+# Class distribution of train data
+table(train$OverallCond)
+
+# Down Sample
+set.seed(100)
+down_train <- downSample(x = train[, colnames(train) %ni% "OverallCon"],
+                         y = train$OverallCond)
+table(down_train$OverallCond)
+
+# Up Sample (optional)
+set.seed(100)
+up_train <- upSample(x = train[, colnames(train) %ni% "OverallCon"],
+                     y = train$OverallCond)
+
+table(up_train$OverallCond)
 
 
-fullMod3 <- glm(OverallCond~., family="binomial", data=credit_train)
 
-mod3 <- glm(OverallCond ~ Condition1 + YearBuilt + Exterior1st + BsmtQual + GrLivArea +
+############## FEATURE SELECTION ##############################
+# FEATURE SELECTION - for Overall Condition rating classification
+fullMod1 <- glm(OverallCond~., family="binomial", data=train)
+
+mod1 <- glm(OverallCond ~ Condition1 + HouseStyle + YearBuilt + Exterior1st + Exterior1st +
+              MasVnrArea + Foundation + TotalBsmtSF + GrLivArea + Functional +GarageArea  +
+              YrSold + SaleType + SalePrice, family="binomial", data=df0)
+
+# Backwards selection is the default
+step1 = step(fullMod1) 
+### Fianl MOdel from step - AIC  1083.33
+stepMod1 <- glm(OverallCond ~ Street + YearBuilt + MasVnrArea + ExterCond + Foundation + 
+                  BsmtQual + BsmtCond + TotalBsmtSF + X1stFlrSF + X2ndFlrSF + 
+                  FullBath + BedroomAbvGr + KitchenQual + Fireplaces + GarageArea + 
+                  GarageCond + YrSold + SalePrice,  family="binomial", data=df0)
+
+
+mod2 <- glm(OverallCond ~ Condition1 + YearBuilt + BsmtQual + GrLivArea +
               Functional + GarageArea + SalePrice + SaleCondition + SaleType + PavedDrive +
-              Fireplaces + GrLivArea + TotalBsmtSF + BldgType, family="binomial", data=credit_train)
+              Fireplaces + GrLivArea + BldgType, family="binomial", data=train)
+
+# Select model with stepAIC
+library("MASS")
+stepAIC <- stepAIC(fullMod1)
+
+mod3 <- glm(OverallCond ~ YearBuilt + Foundation + BsmtQual + TotalBsmtSF + BsmtCond + BsmtQual + X1stFlrSF +
+              X2ndFlrSF + FullBath + BedroomAbvGr + KitchenQual + Fireplaces + YrSold + SalePrice,family="binomial", data=train )
+
+# Build Logistic Model
+#logitmod <- glm(Class ~ Cl.thickness + Cell.size + Cell.shape, family = "binomial", data=down_train)
+logitmod <- glm(OverallCond ~  Condition1 + YearBuilt + BsmtQual + GrLivArea +
+                  Functional + GarageArea + SalePrice + SaleCondition + SaleType + PavedDrive +
+                  Fireplaces + BldgType, family="binomial", data=down_train)
+
+pred <- predict(logitmod, newdata = test, type = "response")
+# Recode factors
+y_pred_num <- ifelse(pred > 0.5, 1, 0)
+y_pred <- factor(y_pred_num, levels=c(0, 1))
+y_act <- testData$OverallCond
+table(pred)
+# Accuracy
+mean(y_pred == y_act)  # 94%
+
+pred <- predict(logitmod, newdata = testData, type = "response")
+
+summary(fullMod1) # AIC 1,165.9
+summary(step)   #AIC  1,083.33
+summary(stepMod)#AIC  1,083.33
+summary(mod1)    #AIC 1,203.9
+summary(mod2)     # AIC = 963.84.62 (removed TotRmsAbvGrd correlated to GrLivArea)
+summary(stepAIC) #AIC = 1083.3
+summary(mod3) # AIC=1083.33
+summary(logitmod) #AIC 94.594
 
 
-summary(fullMod3) # AIC = 952.37.5
-summary(mod3)     # AIC = 983.53
+######Prediction #######
+#install.packages('caTools')
+library(caTools)
+set.seed(88)
+split <- sample.split(df0$OverallCond, SplitRatio = 0.80)
 
+#get training and test data
+dresstrain <- subset(df0, split == TRUE)
+dresstest <- subset(df0, split == FALSE)
 
+summary(logitmod)
+predict <- predict(logitmod, type = 'response')
+#confusion matrix
+table(dresstrain$OverallCond, predict > 0.5)
 
-# Select all the categorical variables
-cat <- unlist(lapply(df0, is.factor))
-cat <- df0[cat]
+#ROCR Curve
+#install.packages("ROCR")
+library(ROCR)
+data(ROCR.simple)
+ROCRpred <- prediction(ROCR.simple$predictions, ROCR.simple$labels)
 
-xtabs(formula = ~., data = cat, subset, sparse = FALSE,
-      na.action, addNA = FALSE, exclude = if(!addNA) c(NA, NaN),
-      drop.unused.levels = FALSE)
+#library(ROCR)
+#ROCRpred <- prediction(predict, dresstrain$OverallCond)
+ROCRperf <- performance(ROCRpred, 'tpr','fpr')
+plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7))
+
 
 
 
 #############################################################################
 ###  QUESTION 3: Predicting House Prices
 #############################################################################
-# FEATURE SELECTION - for Overall Condition rating classification
-fullMod1 <- glm(OverallCond~., family="binomial", data=df0)
 
-mod1 <- glm(OverallCond ~ Condition1 + HouseStyle + YearBuilt + Exterior1st + Exterior1st +
-                  MasVnrArea + Foundation + TotalBsmtSF + GrLivArea + Functional +GarageArea  +
-                  YrSold + SaleType + SalePrice, family="binomial", data=df0)
+#SCALE Numerical variables 
+df_scaled <- df0 %>% mutate_if(is.numeric, scale)
+summary(df_scaled)
+dfCom <- cbind(cat_df, df_scaled)
 
-# Backwards selection is the default
-step1 = step(fullMod) 
-### Fianl MOdel from step - AIC  1083.33
-stepMod1 <- glm(OverallCond ~ Street + YearBuilt + MasVnrArea + ExterCond + Foundation + 
-                             BsmtQual + BsmtCond + TotalBsmtSF + X1stFlrSF + X2ndFlrSF + 
-                             FullBath + BedroomAbvGr + KitchenQual + Fireplaces + GarageArea + 
-                             GarageCond + YrSold + SalePrice,  family="binomial", data=df0)
-
-summary(fullMod1) # AIC 1, 165.9
-summary(mod1)    #AIC 1, 203.9
-summary(step1)   #AIC  1, 083.33
-summary(stepMod1)#AIC  1, 083
 
 
 #############################################################################
 ###  QUESTION 4: Research Question in Relation to House Data
 #############################################################################
-###
-#FEATURE SELECTION 2 - for SalePrice prediction
-##
-
-fullMod2 <- lm(df0$SalePrice~., family="binomial", data=df0)
-summary(fullMod2)
-mod2 <- lm(SalePrice ~ LotArea + Street + LotConfig + Neighborhood + Condition1 + Condition2 +
-             BldgType + HouseStyle + OverallCond + RoofMatl + MasVnrArea + ExterQual + BsmtQual +
-             TotalBsmtSF +GrLivArea + BedroomAbvGr + KitchenAbvGr + KitchenQual + Functional + Fireplaces +
-             GarageArea + SaleType + SaleCondition, family="binomial", data=df0)
-
-# Backwards selection is the default
-step2 = step(fullMod2) 
-### Fianl MOdel from step - AIC  1083.33
-stepMod2 <- lm(df0$SalePrice ~ LotArea + Street + LotConfig + Neighborhood + 
-                  Condition1 + Condition2 + BldgType + HouseStyle + OverallQual + 
-                  OverallCond + YearBuilt + RoofMatl + MasVnrArea + ExterQual + 
-                  BsmtQual + BsmtCond + TotalBsmtSF + X2ndFlrSF + GrLivArea + 
-                  FullBath + BedroomAbvGr + KitchenAbvGr + KitchenQual + Functional + 
-                  Fireplaces + GarageArea + SaleType + SaleCondition,family="binomial", data=df0 )
-
-summary(fullMod2) # A-R^2 = 0.8982 
-summary(mod2)    # A-R^2 = 0.8902 
-summary(step2)   # A-R^2 = 0.8985   AIC = 29710.05
-summary(stepMod2)# A-R^2 = 0.8985 
-
-
-summary(df$SalePrice)
-ggplot(df,aes(x = SalePrice) )+ geom_histogram(bins = 30, fill= 'blue')
-
-#install.packages("gridExtra")
-library("gridExtra")
-
-head(df)
 
 
 
-#########################################################
-library(corrplot)
-data.corr <- as.data.frame(sapply(df, as.numeric))
-
-correlations = cor(data.corr, method = "s")
-# Show variables that have strong correlations with price, focus on coefficient > 0.5 or < -0.5
-corr.price = as.matrix(sort(correlations[,'SalePrice'], decreasing = TRUE))
-corr.id = names(which(apply(corr.price, 1, function(x) (x > 0.05 | x < -0.50))))
-corrplot(as.matrix(correlations[corr.id,corr.id]), type = 'upper', method='color', addCoef.col = 'black', tl.cex = 1,cl.cex = 1, number.cex=1)
 
 
-sapply(df, function(x) length(unique(x)))
+
 
 
 
